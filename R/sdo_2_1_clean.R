c1610eb11134351036eace38de38835b7717b3a9
Jonas Gaigr <jonas.gaigr@gmail.com>
2026-01-19 23:17:53 +0100

    init

---
+++ R/sdo_2_1_clean.R
@@ -0,0 +1,2 @@
+druhy_raw <- read.csv("Outputs/Data/souhrn_druhy.csv", fileEncoding = "Windows-1250")
+stanoviste_raw <- read.csv("Outputs/Data/souhrn_stanoviste.csv", fileEncoding = "Windows-1250")
